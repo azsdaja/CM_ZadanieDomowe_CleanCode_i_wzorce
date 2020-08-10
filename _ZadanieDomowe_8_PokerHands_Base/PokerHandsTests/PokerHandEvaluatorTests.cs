dbb80bc53a88e2698e6bc6bf4890817e3dea5924
JaJezior <63904471+JaJezior@users.noreply.github.com>
2020-08-10 21:36:28 +0200

    Add files via upload

--- _ZadanieDomowe_8_PokerHands_Base/PokerHandsTests/PokerHandEvaluatorTests.cs
+++ _ZadanieDomowe_8_PokerHands_Base/PokerHandsTests/PokerHandEvaluatorTests.cs
@@ -7,36 +7,25 @@ namespace PokerHandsTests
 {
     public class PokerHandEvaluatorTests
     {
-        [Test]
-        public void GetHighestCombination_FourSevens_ReturnsQuads()
-        {
-            var evaluator = new PokerHandEvaluator();
-
-            Combination highestCombination = evaluator.WhatIsTheHighestCombination(123, "H7", "S7", "C7", "CQ", "D7");
-
-            if(highestCombination != Combination.Quads)
-                throw new Exception("not quads");
-        }
-
-        [TestCase("C2", "S5", "D7", "C9", "HQ", Combination.HighCard)]
+        [TestCase("C2", "S3", "D4", "C5", "HQ", Combination.HighCard)]
         [TestCase("C2", "S2", "D7", "C9", "HQ", Combination.Pair)]
         [TestCase("C2", "S2", "D7", "C7", "HQ", Combination.TwoPairs)]
         [TestCase("C2", "S2", "D2", "C9", "HQ", Combination.Three)]
         [TestCase("C2", "S3", "D4", "C5", "H6", Combination.Straight)]
         [TestCase("C2", "C5", "C7", "C9", "CQ", Combination.Flush)]
-        // [TestCase] todo: how should I test a case with Full?
+        [TestCase("S9", "S5", "H5", "C9", "C5", Combination.Full)]
         [TestCase("C2", "S2", "D2", "H2", "HQ", Combination.Quads)]
         [TestCase("H9", "H10", "H7", "H8", "HJ", Combination.StraightFlush)]
         [TestCase("H10", "HJ", "HQ", "HK", "HA", Combination.RoyalFlush)]
         public void GetHighestCombination_FiveCorrectCards_ReturnsHighestCombination
-            (string card1, string card2, string card3, string card4, string card5, Combination expect)
+            (string card1, string card2, string card3, string card4, string card5, Combination expectedCombination)
         {
-            var evaluator = new PokerHandEvaluator();
+            var evaluator = new PokerHandEvaluator(new PokerCardParser());
 
-            Combination x 
-                = evaluator.WhatIsTheHighestCombination(123, card1, card2, card3, card4, card5);
+            Combination accualCombination 
+                = evaluator.GetHighestCombination(123, card1, card2, card3, card4, card5);
 
-            // todo - okay, actually I forgot how to write assertions
+            Assert.AreEqual(expectedCombination, accualCombination);
         }
     }
 }
