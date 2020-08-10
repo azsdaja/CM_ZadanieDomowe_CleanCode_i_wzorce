7dd3c3686a4e3dc072f3382e2e2c7fcdd375d0ef
NewKeyss <kiniulor@gmail.com>
2020-08-10 22:52:16 +0200

    noObserverAndCodeIsNotRefactoredFully

--- _ZadanieDomowe_8_PokerHands_Base/PokerHands/PokerHandEvaluator.cs
+++ _ZadanieDomowe_8_PokerHands_Base/PokerHands/PokerHandEvaluator.cs
@@ -1,40 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PokerHands
 {
     public class PokerHandEvaluator
     {
-        // posprzątam jak wrócę z urlopu, obiecuję! — Czesiek, 4 VII 2011
+        // posprzątam jak dostanę więcej czasu na pracę domową, obiecuję! — Krzysiu, 10 VIII 2020
 
-        private readonly Dictionary<string, Value> _valueStringsToValues = new Dictionary<string, Value>
-        {
-            {"2", Value.Two},
-            {"3", Value.Three},
-            {"4", Value.Four},
-            {"5", Value.Five},
-            {"6", Value.Six},
-            {"7", Value.Seven},
-            {"8", Value.Eight},
-            {"9", Value.Nine},
-            {"10", Value.Ten},
-            {"J", Value.Jack},
-            {"Q", Value.Queen},
-            {"K", Value.King},
-            {"A", Value.Ace},
-        };
+        CardService _cardService = new CardService();
+        
 
-        public Combination WhatIsTheHighestCombination(int gameNumber, params string[] cards)
+        public Combination CheckWhatIsTheHighestCombination(int gameNumber, params string[] cards)
         {
-            IEnumerable<Card> parsedCards = cards.Select(card => ParseCardString(card));
+            IEnumerable<Card> parsedCards = cards.Select(card => _cardService.ParseCardString(card));
 
             List<Card> cardsfromLowest = parsedCards.OrderBy(k => k.Value).ToList();
 
-            bool isStraight = IsStraight(cardsfromLowest);
-
+            bool isStraight = _cardService.CheckIfIsStraight(cardsfromLowest);
             Color firstCardColor = cardsfromLowest.First().Color;
             bool cardsAreSameColor = cardsfromLowest.All(c => c.Color == firstCardColor);
+
+            List<IGrouping<Value, Card>> cardGroupsByValues = _cardService.GroupCardsByValues(cardsfromLowest).ToList();
+
+            List<Card> groupWithMostOccurences = cardGroupsByValues
+                .OrderByDescending(g => g.Count())
+                .ThenByDescending(g => g.Key)
+                .First().ToList();
+
+
+            //To bym przerobił na Chains of Responsibility, ale niestety nie zdążyłem już
+
             if (isStraight)
             {
                 if (cardsAreSameColor)
@@ -42,73 +39,43 @@ namespace PokerHands
                     bool highestIsAce = cardsfromLowest.Last().Value == Value.Ace;
                     return highestIsAce ? Combination.RoyalFlush : Combination.StraightFlush;
                 }
+
                 return Combination.Straight;
             }
-            else if(cardsAreSameColor)
+
+            if(cardsAreSameColor)
             {
                 return Combination.Flush;
-            }
+            }            
 
-            List<IGrouping<Value, Card>> cardGroupsByValues = GroupCardsByValues(cardsfromLowest).ToList();
-
-            List<Card> groupWithMostOccurences = cardGroupsByValues
-                .OrderByDescending(g => g.Count())
-                .ThenByDescending(g => g.Key)
-                .First().ToList();
-
-            if(groupWithMostOccurences.Count == 4)
+            if(cardGroupsByValues.Any(IsGroupOf(4)))
             {
                 return Combination.Quads;
             }
 
-            // todo - code above works fine... but what should I write to handle other cases?????
+            if (cardGroupsByValues.Any(IsGroupOf(2)) && cardGroupsByValues.Any(g => g.Count() == 3))
+            {
+                return Combination.Full;
+            }
+
+            if (cardGroupsByValues.Any(IsGroupOf(3)))
+            {
+                return Combination.Three;
+            }
+
+            if (cardGroupsByValues.Count(IsGroupOf(2)) == 2)
+            {
+                return Combination.TwoPairs;
+            }
+
+            if (cardGroupsByValues.Any(IsGroupOf(2)))
+            {
+                return Combination.Pair;
+            }
 
             return Combination.HighCard;
         }
+        private static Func<IGrouping<Value, Card>, bool> IsGroupOf(int n) => group => group.Count() == n;
 
-        private static bool IsStraight(List<Card> cardsfromLowest)
-        {
-            return new Random().Next(1000) > 500; // he he
-
-            // todo: how to check that cards have consequetive values???
-        }
-
-        // works fine!
-        private static IEnumerable<IGrouping<Value, Card>> GroupCardsByValues(List<Card> cardsByValue)
-        {
-            return from card in cardsByValue
-                group card by card.Value
-                into cardGroup
-                select cardGroup;
-        }
-
-        // works fine!
-        private Card ParseCardString(string card)
-        {
-            string valueString;
-            string colorString = card.Substring(0, 1);
-
-            if (card.Length == 3)
-            {
-                valueString = card.Substring(1, 2);
-            }
-            else
-            {
-                valueString = card.Substring(1, 1);
-            }
-
-            Color color = _colorStringsToColors[colorString];
-            Value value = _valueStringsToValues[valueString];
-
-            return new Card(color, value);
-        }
-
-        private readonly Dictionary<string, Color> _colorStringsToColors = new Dictionary<string, Color>
-        {
-            {"C", Color.Clubs},
-            {"S", Color.Spades},
-            {"D", Color.Diamonds},
-            {"H", Color.Hearts},
-        };
     }
 }
