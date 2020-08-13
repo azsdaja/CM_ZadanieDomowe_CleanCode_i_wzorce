f3cafbbd0a2e33c782f73dd417d513e3ecf0d208
Piotr <example@example.com>
2020-08-13 19:13:50 +0200

    Implemented result logger

---
+++ _ZadanieDomowe_8_PokerHands_Base/PokerHands/Model/GameLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerHands.Model
+{
+    public class GameLog
+    {
+        public int GameNumber { get; set; }
+        public Combination HighestCombination { get; set; }
+
+        public GameLog(int gameNumber, Combination highestCombination)
+        {
+            GameNumber = gameNumber;
+            HighestCombination = highestCombination;
+        }
+    }
+}
---
+++ _ZadanieDomowe_8_PokerHands_Base/PokerHands/Services/PokerHandLogger.cs
@@ -0,0 +1,12 @@
+﻿using PokerHands.Model;
+
+namespace PokerHands.Services
+{
+    public class PokerHandLogger
+    {
+        public GameLog SaveGameResult(int gameNumber, Combination highestCombination)
+        {
+            return new GameLog(gameNumber, highestCombination);
+        }
+    }
+}
